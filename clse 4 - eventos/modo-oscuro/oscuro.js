bfd58ee0259a11b5932ed486423bc98ef3af893f
Lola <tziganelamaga@gmail.com>
2021-05-01 16:55:03 +0200

    funcionamiento del botón modo oscuro en i/v

--- clse 4 - eventos/modo-oscuro/oscuro.js
+++ clse 4 - eventos/modo-oscuro/oscuro.js
@@ -1,42 +1,68 @@
 const bodyCentrado = document.getElementById (`body-centrado`)
-bodyCentrado.style= `display:flex; flex-direction: column; justify-content: center; align-items: center;`
 
 const pSize = document.querySelector (`p`)
-pSize.style= `width: 900px; font-size: 30px; text-align: center;`
 
 const buttonDarkMode = document.querySelector (`button`)
-buttonDarkMode.style= `height: 80px; width:200px; outline: none; border-radius: 35px;`
 
-const sun = document.getElementsByClassName (`fa-sun`)
-const moon = document.getElementsByClassName (`fa-moon`)
-moon.style = `display: none;`
+const sunIcon = document.getElementById (`sun`)
+const moonIcon = document.getElementById (`moon`)
+moonIcon.style = `display: none;`
 
 const h1Text = document.getElementById (`h1-text`) 
-
 
 /* cuando hago click en el botón compruebo en que modo estoy
 y si estoy en modo claro paso a modo oscuro y viceversa
 necesito una variable para guardar en que estado estoy y 
 es la que voy a guardar en el if*/
-
-
 function changeDark () {
-    bodyCentrado.style.backgroundColor = "grey";
-    pSize.style.color = "blue";
+    bodyCentrado.style.backgroundColor = "black";
+    pSize.style.color = "white";
     h1Text.innerHTML= "Modo Oscuro";
-    sun.style= 'display: none';
-    console.log (changeDark)    
+    h1Text.color= "white";
+    sunIcon.style= 'display: none';
+    moonIcon.style= "display: inline";   
 }
 
-buttonDarkMode.addEventListener (`click`, changeDark)
+function changeInicio () {
+    bodyCentrado.style.backgroundColor = "white";
+    pSize.style.color = "black";
+    h1Text.innerHTML= "";
+    sunIcon.style= 'display: inline';
+    moonIcon.style= "display: none";  
+}
 
+let modoPantalla = "claro" 
 
-/*function revealSection() {
-    if (hiddenSection.classList.contains("reveal")) {
-        ;
-    } else {
-        hiddenSection.classList.add("reveal");
+buttonDarkMode.addEventListener (`click`, function() {
+    console.log("aprete botón")
+    console.log(modoPantalla)
+    if (modoPantalla == "oscuro") {
+        // cambia a modo claro
+        changeInicio()
+        modoPantalla = "claro"
     }
-}
-Event listener 
-revealButton.addEventListener("click", revealSection, false);*/
+    else {
+        // cambia a modo oscuro
+        changeDark()
+        modoPantalla = "oscuro"
+    }
+})
+
+// "oscuro" "claro"
+// let estoyEnModoOscuro = false 
+
+
+// buttonDarkMode.addEventListener (`click`, function() {
+//     console.log("aprete botón")
+//     // console.log(modoPantalla)
+//     if (estoyEnModoOscuro) {
+//         // cambia a modo claro
+//         changeInicio()
+//         estoyEnModoOscuro = false
+//     }
+//     else {
+//         // cambia a modo oscuro
+//         changeDark()
+//         estoyEnModoOscuro = true
+//     }
+// })
