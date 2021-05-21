b8ea583bcd4e7b28257aca150c9bbdf5e2eef6ae
Lola <tziganelamaga@gmail.com>
2021-05-21 15:53:02 +0200

    ejercicio 2 Banco

--- clase 6 - js - switch else if/scriptIfElse.js
+++ clase 6 - js - switch else if/scriptIfElse.js
@@ -15,3 +15,27 @@ const compareOddOrEven=()=>
 
 btnNumber.addEventListener('click',compareOddOrEven)
 
+//------------ ejercicio Banco --------------------
+
+const availableMoney = document.getElementById('saved-money')  
+const extractMoney = document.getElementById('extract-money')  
+const btnBank = document.getElementById('btn-bank')  
+const answerBox = document.getElementById('answer-box')  
+
+const getAnswer=()=>
+{
+    const opBank1 = availableMoney.value;
+    const opBank2 = extractMoney.value;
+    const opAnswer = (opBank1-opBank2);
+    //answerBox.innerHTML= opAnswer;
+    if (opAnswer>0){
+        answerBox.innerHTML= `Usted aún dispone de $${opAnswer} en su cuenta.`  
+    }
+    else {
+        answerBox.innerHTML= 'Usted no dispone de suficiente dinero para realizar la extracción'
+        answerBox.style= "border: dashed 2px rgb(224, 52, 52);"
+    }
+    console.log(getAnswer)
+}
+
+btnBank.addEventListener(`mouseover`,getAnswer)
